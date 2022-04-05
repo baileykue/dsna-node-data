839e9f33cc9b21af047227de841ccbd845859fc0
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-04-05 15:49:48 -0700

    writing out add node and get liest for binary tree node

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+  }
+
+  add(node) {
+    if (!this.next) {
+      this.left = node;
+    } else {
+      this.next.add(node);
+    }
+  }
+
+  getList() {
+    if (!this.next) return this.value;
+    return ` ${this.value} ${this.next.getList()}`;
+  }
+}
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+B.add(A);
+B.add(D);
+B.add(C);
